9ee8b26b4f04e3c8b551e48e3f846631f1aa7b2a
TruongCa95 <57616435+TruongCa95@users.noreply.github.com>
2020-04-18 11:18:26 +0700

    update License

--- SmartNewsDemo/App.xaml.cs
+++ SmartNewsDemo/App.xaml.cs
@@ -8,6 +8,8 @@ namespace SmartNewsDemo
     {
         public App()
         {
+            //Register Syncfusion license
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjQyNDY5QDMxMzgyZTMxMmUzMG5xNjQ4YVhXdEdPaG00U1NsdXZBcFlGZmlvUm1qeThQYVFoclhTV2FaN3c9");
             InitializeComponent();
             MainPage = new NavigationPage(new SmartNewsDemo.View.TabView());
             //MainPage = new SmartNewsDemo.View.NewsTabEx();
